19771db4b2c8ea4a553695b3f0b5f7ebac60f572
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-19 16:25:49 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -14,25 +14,3 @@ def my_all?(collection)
      true
    end
 end
-
-# it "returns false when the block condition is not met" do
-#   expect(my_all?(collection) {|i| i < 3}).to eq(false)
-# end
-
-
-# it "yields the correct element to the block" do
-#   yielded = []
-#   my_all?(collection) do |number|
-#     yielded.push number
-#   end
-#   expect(yielded).to eq([1,2,3])
-# end
-#
-# it "returns false when the block condition is not met" do
-#   expect(my_all?(collection) {|i| i < 3}).to eq(false)
-# end
-#
-# it "returns true when the block condition is met" do
-#   expect(my_all?(collection) {|i| i < 4}).to eq(true)
-# end
-# end
